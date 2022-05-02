57e08ec793e615d319ee1443c919ad6e8064c786
SilvaMercedes <102375776+SilvaMercedes@users.noreply.github.com>
2022-05-02 12:23:28 -0300

    Update fraude.py

--- fraude.py
+++ fraude.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 import numpy
 
 #url = "C:/Users/Horacio/Downloads/diplo csv/creditcard.csv"
-df = pd.read_csv('C:/Users/Horacio/Downloads/diplo csv/creditcard.csv')
+df = pd.read_csv('https://github.com/SilvaMercedes/diplo/blob/main/dataset/fraud_data_pt1.csv')
 df 
