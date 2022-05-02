38ccc90508419ecbf0f449b9eb3486bc6fc9314a
SilvaMercedes <102375776+SilvaMercedes@users.noreply.github.com>
2022-05-02 12:21:48 -0300

    Add files via upload

---
+++ fraude.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy
+
+#url = "C:/Users/Horacio/Downloads/diplo csv/creditcard.csv"
+df = pd.read_csv('C:/Users/Horacio/Downloads/diplo csv/creditcard.csv')
+df 
